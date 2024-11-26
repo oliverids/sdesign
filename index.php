94a9c2a36a0f0db96e74a96cf45a0e52e67af1ae
Isabela Oliveira <oliver.isabela2303@gmail.com>
2024-11-26 18:26:32 -0300

    ops: clarity

--- index.php
+++ index.php
@@ -272,11 +272,10 @@
     </div>
   </section>
 
+  <footer></footer>
 
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
   <script src="scripts/main.js"></script>
-
-  <footer></footer>
 </body>
 
 </html>
