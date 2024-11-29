0e3feba2a91bf92d5ef67edcabc0f6a8b1b27936
Isabela Oliveira <oliver.isabela2303@gmail.com>
2024-11-29 15:18:57 -0300

    feat: meta tags - seo

---
+++ components/header-meta.php
@@ -0,0 +1,19 @@
+<meta name="description" content="O SDesign, da UFES, promove integração e aprendizado com palestras, oficinas e rodas de conversa para designers e admiradores.">
+<meta name="keywords" content="sdesign, semana do design, 2024, ufes, palestra, oficina, design, designer, perspectivas, hub, hub es">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="revisit-after" content="10 days">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://metatags.io/" />
+<meta property="og:title" content="SDESIGN 2024 — Semana do Design" />
+<meta property="og:description" content="O SDesign, organizado por estudantes de Design UFES, promove integração entre designers, profissionais e admiradores com palestras, oficinas e rodas de conversa, incentivando aprendizado e conexões." />
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://metatags.io/" />
+<meta property="twitter:title" content="SDESIGN 2024 — Semana do Design" />
+<meta property="twitter:description" content="O SDesign, organizado por estudantes de Design UFES, promove integração entre designers, profissionais e admiradores com palestras, oficinas e rodas de conversa, incentivando aprendizado e conexões." />
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SDESIGN 2024 - Perspectivas</title>
   <link rel="stylesheet" href="css/index.min.css">
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <title>SDESIGN 2024 — Semana do Design</title>
+
+  <?php require_once('./components/header-meta.php'); ?>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
