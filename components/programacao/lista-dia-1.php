d448172252e0fb3c44e632a851873856cb9a9f48
Wallace027Dev <wallaceofc@hotmail.com>
2025-10-27 20:16:59 -0300

    feat(eventos): lista 2 de eventos

--- components/programacao/lista-dia-1.php
+++ components/programacao/lista-dia-1.php
@@ -1,38 +1,39 @@
 <li class="programacao-item">
   <div class="programacao-hora">
-    <span>09h às 12h</span>
+    <span>09h às 10h</span>
   </div>
 
   <?php
-  $palestrante = 'DNA';
-  $palestra_titulo = 'Stop-motion: Reciclando Ideias';
-  $palestra_tag = 'Oficina';
-  $palestra_descricao = 'Crie animações incríveis com materiais recicláveis! Nesta oficina de stop-motion, você aprenderá teoria, roteiro, produção e animação na prática. Trabalhe em equipe para criar narrativas únicas, dando vida a objetos reciclados em animações colaborativas de 30 segundos.';
+  $palestrante = 'Letícia Pedruzzi';
+  $palestra_titulo = 'Revistas Capixabas - Um olhar sobre a cultura impressa no Espírito Santo';
+  $palestra_tag = 'Sala Criativa';
+  $palestra_descricao = 'Nesta palestra, será apresentado o trabalho desenvolvido pelo LAD — Laboratório de Design — em torno da cultura impressa no Espírito Santo, com foco nas revistas capixabas e no papel que elas desempenharam na construção da identidade local. A fala propõe um percurso que conecta história e design gráfico, explorando publicações como espaços de circulação de ideias, imagens e modos de representar o cotidiano.';
   include('components/programacao-conteudo.php')
   ?>
 </li>
 
 <li class="programacao-item">
   <div class="programacao-hora">
-    <span>09h às 10h</span>
+    <span>09h às 12h</span>
   </div>
 
   <div class="conteudo-linha">
     <?php
-    $palestrante = 'Rafael Pessin';
-    $palestra_titulo = 'Vídeos que tocam o coração: como criar emoções através de vídeos criativos';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Descubra o poder da emoção nos vídeos criativos! Aprenda técnicas e fundamentos para criar vídeos que tocam o coração, despertam lembranças e garantem mensagens impactantes. Transforme suas produções em ferramentas assertivas e memoráveis no mercado competitivo.';
+    $palestrante = 'TEM Design';
+    $palestra_titulo = 'Análise de Portfólios em Design';
+    $palestra_tag = 'Lab Make';
+    $palestra_descricao = 'Um momento de troca e aprendizado voltado para quem deseja aprimorar seus portfólios. A atividade oferece feedbacks de designers experientes, abordando aspectos como apresentação visual, clareza conceitual, processo criativo e narrativa dos projetos. O objetivo é ajudar os participantes a comunicar melhor suas ideias, fortalecer sua identidade profissional e se preparar para o mercado, seja para vagas em estúdios, agências ou trabalhos autorais.
+';
     include('components/programacao-conteudo.php')
     ?>
 
-    <!-- <?php
-          $palestrante = 'Amanda Lobos';
-          $palestra_titulo = 'Ilustração: possibilidades e carreira';
-          $palestra_tag = 'Palestra';
-          $palestra_descricao = 'Explore o mercado de ilustração e suas conexões com o design! Entenda como transformar sua paixão em uma carreira independente ou integrada, dominando desde a prospecção de clientes até a entrega. Descubra novas formas de trabalhar e crescer como ilustrador!';
-          include('components/programacao-conteudo.php')
-          ?> -->
+    <?php
+    $palestrante = 'Zoy Anastassakis 9 as 10 wallacew muda';
+    $palestra_titulo = 'Atos cotidianos de design: aprendendo em tempos de urgência';
+    $palestra_tag = 'Lab Maker';
+    $palestra_descricao = 'Nesta palestra, apresentando o livro “Atos cotidianos de design: aprendendo em tempos de urgência", coescrito com Marcos Martins, Zoy Anastassakis problematizará as teorias de projeto em design, propondo que o fazer design pode ser repensando como um conjunto de aprendizados vinculados às demandas do tempo presente, cotidiano. Essa discussão culminará com a apresentação das questões de interesse da linha de pesquisa "Design, Territorialidades e Antropoceno” do PPDESDI/UERJ, à qual as pesquisas de Zoy Anastassakis estão vinculadas';
+    include('components/programacao-conteudo.php')
+    ?>
   </div>
 </li>
 
@@ -43,18 +44,18 @@
 
   <div class="conteudo-linha">
     <?php
-    $palestrante = 'Katia Miller (Docente UFES)';
-    $palestra_titulo = 'Reciclagem criativa do plástico';
-    $palestra_tag = 'Oficina';
-    $palestra_descricao = 'Transforme plástico reciclado em utilitários criativos! Aprenda sobre upcycling e economia circular enquanto cria objetos como carteiras ou estojos a partir de sacolas plásticas recicladas. Desenvolva novos hábitos de consumo e contribua para um futuro mais sustentável.';
+    $palestrante = 'Ronaldo Barbosa';
+    $palestra_titulo = 'Trajetória profissional - 1° Designer capixaba';
+    $palestra_tag = 'Sala Criativa';
+    $palestra_descricao = 'a palestra, Ronaldo Barbosa, designer responsável por marcas icônicas do nosso estado, como Banestes e Águia Branca. fala da sua trajetória e recentes preocupações (trabalhos recentes)';
     include('components/programacao-conteudo.php')
     ?>
 
     <?php
-    $palestrante = 'Marcos Spinassé';
-    $palestra_titulo = 'Design social no design de joias';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Descubra o universo do design de joias e sua conexão com o impacto social! Conheça o Projeto Jóias da Barra, que uniu o LabDesignJoia e o Coletivo Barra de Renda para criar joias com propósito. Apresentado na Feninjer 2024, a palestra explora o design social e tendências do setor.';
+    $palestrante = 'Nathalia Domitrovic';
+    $palestra_titulo = 'Design em Movimento: Pesquisa, Prática e IA';
+    $palestra_tag = 'Auditório';
+    $palestra_descricao = 'Na palestra, Nathalia vai compartilhar sua experiência em adaptar conhecimentos acadêmicos de pesquisa qualitativa para o contexto dos negócios. O foco será em exemplos práticos e reflexões que tem feito ao longo da sua carreira, na busca constante em se manter relevante num contexto tão competitivo e acelerado. Sem a pretensão de trazer respostas definitivas, serão exploradas questões como: qual o nosso papel como designers em times multidisciplinares de tecnologia e negócio? Como fazer a diferença e trazer valor, com cada vez mais pressão e prazos mais curtos? O que faz sentido flexibilizar e o que é inegociável na nossa prática profissional? Como a IA vem mudando e continuará a mudar essa realidade nos próximos anos?';
     include('components/programacao-conteudo.php')
     ?>
   </div>
@@ -64,31 +65,37 @@
 
 <li class="programacao-item">
   <div class="programacao-hora">
-    <span>13h30 às 14h30</span>
+    <span>13h30 às 16h30</span>
   </div>
 
   <div class="conteudo-linha">
     <?php
-    $palestrante = 'Roberlan (This is Blasé Studio)';
-    $palestra_titulo = '“De Vitória para o Mundo - Alcançando o Mercado Internacional”';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Inspire-se com uma trajetória que levou talentos locais ao mercado internacional! Conheça histórias, estratégias e dicas práticas para conquistar clientes globais, expandir sua carreira e mostrar ao mundo o potencial criativo do Espírito Santo.';
+    $palestrante = 'Natália Bastos';
+    $palestra_titulo = '“Primeiros Passos em Jogos Digitais”';
+    $palestra_tag = 'Lab Maker';
+    $palestra_descricao = 'Na oficina Primeiros Passos em Jogos Digitais, quero compartilhar como a programação pode ser uma ferramenta acessível que dá autonomia para criar jogos. A proposta é apresentar conceitos iniciais de lógica e mostrar caminhos práticos para que designers consigam transformar suas ideias em pequenos experimentos interativos. Os participantes vão conhecer noções básicas de programação aplicadas ao design e experimentar, de forma guiada, como esses conceitos podem ser usados em pequenos testes práticos. A ideia é oferecer uma introdução simples e acessível, que mostre caminhos para quem quiser continuar explorando a criação de jogos digitais.
+';
     include('components/programacao-conteudo.php')
     ?>
 
     <?php
-    $palestrante = 'Leonardo Zamprogno';
-    $palestra_titulo = 'Game Design como ferramenta social de inclusão e sustentabilidade';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Descubra como o game design pode ser uma ferramenta de inclusão e impacto social! Conheça jogos que promovem cultura, combatem o racismo e incentivam a adoção de animais, além de dicas para criar projetos relevantes e concorrer em editais culturais com sucesso.';
+    $palestrante = 'João Pitanga';
+    $palestra_titulo = 'Análise de Portfólios em Publicidade';
+    $palestra_tag = 'Auditório';
+    $palestra_descricao = 'Uma oportunidade para quem quer  se destacar no mercado publicitário. A atividade promove uma troca direta com profissional da área, que analisa os trabalhos apresentados e oferece orientações sobre criatividade, posicionamento, clareza estratégica e narrativa dos projetos. Mais do que uma avaliação, é um bate-papo sobre carreira, repertório e mercado, ajudando cada participante a entender como comunicar seu potencial de forma autêntica e impactante.
+';
     include('components/programacao-conteudo.php')
     ?>
 
     <?php
-    $palestrante = 'Paula Gobetti';
-    $palestra_titulo = 'As letras precisam dançar!';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Redescubra o design com perspectivas indígenas e decoloniais! Entenda como criar materiais gráficos respeitando saberes originários e rompendo com paradigmas ocidentais dominantes. Amplie sua visão sobre práticas, metodologias e formas de viver e compartilhar conhecimentos.';
+    $palestrante = 'Cris Ibarra';
+    $palestra_titulo = 'CUIDA: práticas e experiências participativas de design e antropologia';
+    $palestra_tag = 'Auditório';
+    $palestra_descricao = 'O CUIDA é um laboratório em formação que nasce do encontro entre design, antropologia e práticas colaborativas de criação, escuta e cuidado. O coletivo busca articular abordagens participativas, saberes pluriversais e experiências corporais para cultivar outros modos de pensar e fazer design.
+
+Na palestra “CUIDA: práticas e experiências participativas de design e antropologia”, apresento alguns dos trabalhos de pesquisa, ensino e extensão desenvolvidos pelo grupo, exponho nossos métodos e eixos de atuação e, sobretudo, abro espaço para refletirmos coletivamente sobre nossos posicionamentos e escolhas como pesquisadores em design.
+
+Termos recorrentes na área — como solução, problema, projeto, usuário ou aplicação — carregam visões de mundo que, muitas vezes, limitam ou distorcem as experiências vividas pelas pessoas e comunidades com as quais trabalhamos. Afinal, o design surge de lógicas da modernidade que tendem a uniformizar e reduzir a pluralidade.';
     include('components/programacao-conteudo.php')
     ?>
   </div>
@@ -101,27 +108,36 @@
 
   <div class="conteudo-linha">
     <?php
-    $palestrante = 'Anclebio Jr';
-    $palestra_titulo = 'Tem Folia Neste Design';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Mergulhe na conexão entre design e carnaval! Explore como metodologias de design transformam ideias abstratas em desfiles memoráveis, equilibrando criatividade, recursos e sustentabilidade. Descubra novas formas de integrar produção acadêmica à cultura e à sociedade!';
+    $palestrante = 'fEEEio Lab';
+    $palestra_titulo = 'fEEEio Lab: Estética em Tramas e Tipos';
+    $palestra_tag = 'Sala Criativa';
+    $palestra_descricao = 'O fEEEio Lab – ou Laboratório de Estudo e Experimentação da Estética do Feio – é uma proposta de grupo de estudo que busca promover debates estético sociais na cidade do Recife.
+Explorando como a estética influencia nossas escolhas diárias e a sua conexão com a atividade criativa – em especial no campo do Design, Moda, Artes Plásticas e Artesanato – abordamos essas temáticas juntando teoria e prática em rodas de conversas e atividades manuais.
+Para o SDesign vamos juntar a tecelagem livre com exercício de tipografia experimental incentivando os participantes a debaterem sobre a sociedade que vivemos e o seu próprio exercício criativo coletivo.';
     include('components/programacao-conteudo.php')
     ?>
 
+  </div>
+</li>
+
+<li class="programacao-item">
+  <div class="programacao-hora">
+    <span>15h às 17h00</span>
+  </div>
+
+  <div class="conteudo-linha">
     <?php
-    $palestrante = 'Andre Rios';
-    $palestra_titulo = 'Design Gráfico e Efeitos Visuais – Conexões e Possibilidades';
-    $palestra_tag = 'Palestra';
-    $palestra_descricao = 'Explore como o design gráfico impulsiona a criação de efeitos visuais incríveis! Aprenda a combinar composição, cores, fotografia e narrativa para projetos de cinema, games e publicidade. Conecte teoria e prática e amplie sua atuação no universo dos VFX!';
+    $palestrante = 'Rodrigo Saiani';
+    $palestra_titulo = 'Variable Brand Voice';
+    $palestra_tag = 'Auditório SEBRAE';
+    $palestra_descricao = 'Fontes variáveis vieram para ficar. Para a comunidade de apaixonados por tipografia é uma mudança radical e bem-vinda, com tanta dificuldades técnicas quanto possibilidades criativas. Nas empresas, porém, são ilustres desconhecidas. Entra a era das Vozes Variáveis de Marca – Variable Brand Voice®. Numa era em que as marcas buscam interações um-a-um com os clientes, todos os pontos de contato contam.
+Mas há uma tendência rolando na revitalização das marcas que vai contra essa experiência 1 a 1 e deixando as marcas mais aguadas e sem personalidade - um processo que chamamos ironicamente de Blanding®.
+E se um viciado em Coca-Cola pudesse no futuro escolher a quantidade exata de açúcar na sua latinha e recebesse, via drone, em casa, uma lata totalmente personalizada para essa quantidade?
+E uma revista como a Vogue, poderia fazer curadoria tét-a-tét e personalizar layout, capa, conteúdo e logotipo para cada pessoa, individualmente?
+Nesses experimentos, provocamos o pensamento criativo dos amantes de marcas, unindo tipografia "cutting-edge" e branding em uma palestra divertida e instrutiva.
+';
     include('components/programacao-conteudo.php')
     ?>
 
-    <?php
-    $palestrante = 'Design Improvável: Reinventando Conexões entre Pessoas e Natureza';
-    $palestra_titulo = 'Mediadora: Paula Gobetti <br/> Convidados: Cris Ibarra + Moço';
-    $palestra_tag = 'Mesa Redonda';
-    $palestra_descricao = 'Exploraremos como o design improvável pode gerar soluções criativas que fortalecem as relações sociais e ambientais, desafiando convenções e propondo novas formas de conectar comunidades e o meio ambiente de maneira inovadora e decolonial.';
-    include('components/programacao-conteudo.php')
-    ?>
   </div>
 </li>
